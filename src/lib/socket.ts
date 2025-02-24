9f089357e2268e6e788429c8725f37c1805517ff
Brody Nelson <127979848+brodynelly@users.noreply.github.com>
2025-02-24 10:26:15 -0600

    Update socket.ts

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client'
 
-const SOCKET_URL = process.env.REACT_APP_API_URL
+const SOCKET_URL = "ws://paal-tgbf.onrender.com"
 
 export const socket = io(SOCKET_URL, {
   transports: ['websocket'],
